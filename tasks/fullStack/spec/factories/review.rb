ade166976d7ef37796f47017399814f83abeb62a
Robson Peacock <robson_peacock20@outlook.com>
2024-07-26 01:54:37 +0100

    Add average rating functionality

--- tasks/fullStack/spec/factories/review.rb
+++ tasks/fullStack/spec/factories/review.rb
@@ -1,9 +1,26 @@
 FactoryBot.define do
+  sequence(:predefined_number) do |n|
+    numbers = (1..5).to_a.cycle.take(30)
+    numbers[n % 30]
+  end
+
   factory :review do
     body { 'Great product!' }
-    rating { 4.0 }
+    rating { generate(:predefined_number) }
     tags { nil }
     reviewer_name { 'Robson Peacock' }
     product
+
+    trait :last_month do
+      created_at { 1.month.ago }
+    end
+
+    trait :two_months_ago do
+      created_at { 2.months.ago }
+    end
+
+    trait :three_months_ago do
+      created_at { 3.months.ago }
+    end
   end
 end
